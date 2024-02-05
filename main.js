185d7e8fe99827da8f0523cc121bfd61a85c93d7
kareem nour <kareemnour221@gmail.com>
2024-02-05 15:24:15 +0200

    committed

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const video = document.getElementById("video");
 
 function setVideo() {
@@ -6,16 +5,20 @@ function setVideo() {
     video.style.zIndex = "-1";
 }
 
-let name = ""
-document.getElementById("name").addEventListener("keyup",(e) => {
-    if (e.key === "Enter")
-    {
+let name = "";
+document.getElementById("name").addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
         name = document.getElementById("name").value;
         document.body.innerHTML = "";
-        page1(name)
+        page1(name);
     }
-})
+});
 
+document.getElementById("go").addEventListener("click", () => {
+    name = document.getElementById("name").value;
+    document.body.innerHTML = "";
+    page1(name);
+});
 
 function page1(e) {
     setVideo();
@@ -28,33 +31,34 @@ function page1(e) {
     content.innerHTML += ` Are You Alone?`;
 
     window.addEventListener("resize", () => {
-        window.innerWidth < 768 ? content.style.fontSize = "60px": content.style.fontSize = "100px";
-    })
+        window.innerWidth < 768
+            ? (content.style.fontSize = "60px")
+            : (content.style.fontSize = "100px");
+    });
 
     interval(content);
-    
+
     document.body.appendChild(content);
 }
 
 function interval(e) {
     let show = setInterval(() => {
         e.innerHTML = `You Ready?`;
-        e.style.webkitAnimation= "show 7s 2";
+        e.style.webkitAnimation = "show 7s 2";
         clearInterval(show);
-        
+
         let showPage = setInterval(() => {
             document.body.removeChild(e);
             page2();
             clearInterval(showPage);
         }, 7000);
-
     }, 7000);
 }
 
 function page2() {
     let con = document.createElement("div");
     con.classList.add("center");
-    
+
     let cardCon = document.createElement("div");
     cardCon.classList.add("card-con");
 
@@ -63,20 +67,19 @@ function page2() {
     title.style.animationName = "";
     title.innerHTML = "Pick A Side";
 
-    CreateCard("red",cardCon);
-    CreateCard("blue",cardCon);
+    CreateCard("red", cardCon);
+    CreateCard("blue", cardCon);
 
     con.append(cardCon);
-    document.body.append(title,con);
-    
+    document.body.append(title, con);
 }
 
-function CreateCard(color,con) {
+function CreateCard(color, con) {
     let cardBody = document.createElement("div");
-    cardBody.setAttribute('class', 'card-body');
+    cardBody.setAttribute("class", "card-body");
     cardBody.innerHTML = "?";
     cardBody.style.borderColor = color;
-    
+
     con.appendChild(cardBody);
     document.body.style.display = "block";
     document.body.style.height = "unset";
@@ -84,21 +87,16 @@ function CreateCard(color,con) {
     click(cardBody);
 }
 
-
 function click(e) {
-    e.addEventListener("click",() => {
+    e.addEventListener("click", () => {
         if (e.style.borderColor == "red") {
-            
             setTimeout(() => {
-                window.location.assign("./red.html")
+                window.location.assign("./red.html");
             }, 1000);
-            
-        }else if (e.style.borderColor == "blue"){
+        } else if (e.style.borderColor == "blue") {
             setTimeout(() => {
-                window.location.assign("./blue.html")
+                window.location.assign("./blue.html");
             }, 1000);
         }
-    })
+    });
 }
-
-
